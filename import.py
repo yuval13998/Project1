07f12bcdf571253ff670211822c6fc0d3f9b6371
yuval13998 <yuval13998@gmail.com>
2020-03-31 12:37:02 +0300

    latest version

---
+++ import.py
@@ -0,0 +1,8 @@
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
--- models.py
+++ models.py
@@ -4,3 +4,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+
+class Book(db.Model):
+    __tablename__ = "books"
+    isbn = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    reviews = db.relationship("Review", backref="Book", lazy=True)
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String, nullable=False)
+    rate = db.Column(db.Integer, nullable=False)
+    book_isbn = db.Column(db.String, db.ForeignKey("books.isbn"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+class Like(db.Model):
+    __tablename__ = "likes"
+    id = db.Column(db.Integer, primary_key=True)
+    book_isbn = db.Column(db.String, db.ForeignKey("books.isbn"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
