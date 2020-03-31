a34768f39b33fc8b9e394feff7b3da8af24872fd
yuval13998 <yuval13998@gmail.com>
2020-03-31 18:34:10 +0300

    fix create problem

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class User(db.Model):
 
 class Book(db.Model):
     __tablename__ = "books"
-    isbn = db.Column(db.String, nullable=False)
+    isbn = db.Column(db.String, primary_key=True)
     title = db.Column(db.String, nullable=False)
     author = db.Column(db.String, nullable=False)
     year = db.Column(db.Integer, nullable=False)
